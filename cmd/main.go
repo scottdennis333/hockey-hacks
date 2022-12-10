96dc133a84869fdebef8b1cff571ee73359dc017
scottdennis333 <scott.dennis333@gmail.com>
2022-12-10 14:51:31 -0600

    linting and .env.example

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,18 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"encoding/xml"
-	"github.com/joho/godotenv"
-	"golang.org/x/oauth2/endpoints"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/smtp"
 	"net/url"
 	"os"
 	"strings"
 	"sync"
 	"time"
-	"net/smtp"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/endpoints"
 )
 
 type Goalie struct {
@@ -42,10 +43,10 @@ type Game struct {
 }
 
 type YahooAuth struct {
-	AccessToken	 string `json:"access_token"`
+	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
-	ExpiresIn 	 int	`json:"expires_in"`
-	TokenType 	 string	`json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	TokenType    string `json:"token_type"`
 }
 
 type AddPlayer struct {
@@ -54,11 +55,11 @@ type AddPlayer struct {
 }
 
 type AddPlayers struct {
-	XMLName 	 xml.Name `xml:"fantasy_content"`
-	Roster struct {
+	XMLName xml.Name `xml:"fantasy_content"`
+	Roster  struct {
 		CoverageType string `xml:"coverage_type"`
-		Date		 string `xml:"date"`
-		Players	struct {
+		Date         string `xml:"date"`
+		Players      struct {
 			Player []AddPlayer `xml:"player"`
 		} `xml:"players"`
 	} `xml:"roster"`
@@ -79,14 +80,14 @@ var requestBody AddPlayers
 func main() {
 	godotenv.Load("../.env")
 
-	f, err := os.OpenFile("../mylog.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	f, err := os.OpenFile("../mylog.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("error opening file: %v", err)
 	}
 	defer f.Close()
 
 	log.SetOutput(f)
-	
+
 	log.Println("Starting Program")
 
 	wg.Add(1)
@@ -100,7 +101,7 @@ func main() {
 
 func yahooRefreshAuth() {
 	defer wg.Done()
-	tok := base64.StdEncoding.EncodeToString([]byte(os.Getenv("YAHOO_CLIENT_ID")+":"+os.Getenv("YAHOO_CLIENT_SECRET")))
+	tok := base64.StdEncoding.EncodeToString([]byte(os.Getenv("YAHOO_CLIENT_ID") + ":" + os.Getenv("YAHOO_CLIENT_SECRET")))
 
 	data := url.Values{}
 	data.Set("grant_type", "refresh_token")
@@ -114,15 +115,15 @@ func yahooRefreshAuth() {
 	}
 
 	req.Header = http.Header{
-		"Authorization": {"Basic "+tok},
-		"Content-Type": {"application/x-www-form-urlencoded"},
+		"Authorization": {"Basic " + tok},
+		"Content-Type":  {"application/x-www-form-urlencoded"},
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
@@ -145,7 +146,7 @@ func startingGoalies() {
 	}
 
 	req.Header = http.Header{
-		"Content-Type": {"application/json"},
+		"Content-Type":              {"application/json"},
 		"Ocp-Apim-Subscription-Key": {os.Getenv("SPORTS_DATA_KEY")},
 	}
 	resp, err := client.Do(req)
@@ -188,28 +189,28 @@ func yahooSwapPlayers() {
 	requestBody.Roster.CoverageType = "date"
 	requestBody.Roster.Date = time.Now().Format("2006-01-02")
 
-	if (staringGoalies.NJ == Goalie{} && staringGoalies.BOS == Goalie{}){
-		return;
+	if (staringGoalies.NJ == Goalie{} && staringGoalies.BOS == Goalie{}) {
+		return
 	}
 	if (staringGoalies.NJ == Goalie{}) {
 		LU.Position = "G"
 		JS.Position = "G"
 		VN.Position = "BN"
 		AS.Position = "BN"
-	} else if(staringGoalies.BOS == Goalie{}) {
+	} else if (staringGoalies.BOS == Goalie{}) {
 		LU.Position = "BN"
 		JS.Position = "BN"
 		VN.Position = "G"
 		AS.Position = "G"
 	} else {
-		if (staringGoalies.NJ.LastName == "Vanecek") {
+		if staringGoalies.NJ.LastName == "Vanecek" {
 			VN.Position = "G"
 			AS.Position = "BN"
 		} else {
 			VN.Position = "BN"
 			AS.Position = "G"
 		}
-		if (staringGoalies.BOS.LastName == "Ullmark") {
+		if staringGoalies.BOS.LastName == "Ullmark" {
 			LU.Position = "BN"
 			JS.Position = "G"
 		} else {
@@ -227,14 +228,14 @@ func yahooSwapPlayers() {
 	}
 
 	client := http.Client{}
-	yahooUrl := "https://fantasysports.yahooapis.com/fantasy/v2/team/419.l.6795.t."+os.Getenv("YAHOO_TEAM_ID")+"/roster"
-	req, err := http.NewRequest("PUT", yahooUrl,  w)
+	yahooUrl := "https://fantasysports.yahooapis.com/fantasy/v2/team/419.l.6795.t." + os.Getenv("YAHOO_TEAM_ID") + "/roster"
+	req, err := http.NewRequest("PUT", yahooUrl, w)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	req.Header = http.Header{
-		"Authorization": {"Bearer "+yahooAuth.AccessToken},
-		"Content-Type": {"application/xml"},
+		"Authorization": {"Bearer " + yahooAuth.AccessToken},
+		"Content-Type":  {"application/xml"},
 	}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -248,26 +249,25 @@ func yahooSwapPlayers() {
 	log.Println("Ending Program\n")
 }
 
-func sendEmail (respBody []byte) {
- 	from := os.Getenv("EMAIL_ADDRESS")
-    password := os.Getenv("EMAIL_PASSWORD")
+func sendEmail(respBody []byte) {
+	from := os.Getenv("EMAIL_ADDRESS")
+	password := os.Getenv("EMAIL_PASSWORD")
 
-    toEmailAddress := os.Getenv("EMAIL_ADDRESS")
-    to := []string{toEmailAddress}
+	toEmailAddress := os.Getenv("EMAIL_ADDRESS")
+	to := []string{toEmailAddress}
 
-    host := "smtp.gmail.com"
-    port := "587"
-    address := host + ":" + port
+	host := "smtp.gmail.com"
+	port := "587"
+	address := host + ":" + port
 
-    subject := "Subject: Yahoo Refresh Token Failure\n"
-    body := "YAHOO REFRESH TOKEN FAILURE\n\n\nResponse:\n" + string(respBody)
-    message := []byte(subject + body)
+	subject := "Subject: Yahoo Refresh Token Failure\n"
+	body := "YAHOO REFRESH TOKEN FAILURE\n\n\nResponse:\n" + string(respBody)
+	message := []byte(subject + body)
 
-    auth := smtp.PlainAuth("", from, password, host)
+	auth := smtp.PlainAuth("", from, password, host)
 
-    err := smtp.SendMail(address, auth, from, to, message)
-    if err != nil {
-        panic(err)
-    }
+	err := smtp.SendMail(address, auth, from, to, message)
+	if err != nil {
+		panic(err)
+	}
 }
-
