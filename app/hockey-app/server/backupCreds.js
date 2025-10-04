f433fab932a6cd9da225fe2df55b52e6cfc91c98
Scott Dennis <scott.dennis@boldcommerce.com>
2025-10-03 19:27:13 -0500

    remove app

--- app/hockey-app/server/backupCreds.js
+++
@@ -1,9 +0,0 @@
-const clientId = "dj0yJmk9bnBnWXA5OUFhdUNXJmQ9WVdrOVUzWnphemhVVWtJbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTY4";
-const clientSecret = "a80025811fcc0693e722859d7bf6f02320d1b90e";
-const refreshToken = 'AOvrGGWdy0cs6Gpsf0Gukffn0eUb~000~6HqGObV0E8DEFqB8cddp9eia';
-
-
-// const clientId = "dj0yJmk9RnFVaDNkSVdhdUk3JmQ9WVdrOVNWRndhWE5QYm1ZbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWNl";
-// const clientSecret = "887ddd9bbf8d965cddadfbe938344a6f48666a88";
-// const refreshToken = 'AOTYEWUW2gT48Ga.x6ji9EzfA2BX~000~I1NTTvhCs1MvmpiX4QKitF2y';
-
